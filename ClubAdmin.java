d89c1b87e3b3c41c9180b58cf9f753ab460b0c3a
Marii Boyken <mboyken@calpoly.edu>
2016-04-24 14:52:01 -0700

    Initial commit for ClubAdmin class.

---
+++ ClubAdmin.java
@@ -0,0 +1,102 @@
+/**
+ * Class for ClubAdmin.
+ * @author mboyken
+ */
+
+import java.util.*;
+
+public class ClubAdmin extends User
+{
+   private int adminID;
+
+   public ClubAdmin(int adminID, String name, String phoneNum,
+      String empl, String email)
+   {
+      this.adminID = adminID;
+      this.super(name, phoneNum, empl, email);
+   }
+
+   /**
+    * Method to create an Event for the club.
+    * @param start The start time of the event.
+    * @param end The end time of the event.
+    * @param loc The location of the event.
+    * @param descr The description of the event.
+    * @return Returns the created Event.
+    */
+   private Event createEvent(String start, String end, String loc, String descr)
+   {
+      return new Event(start, end, loc, descr);
+   }
+
+   /**
+    * Method to create a club.
+    * @param name The name of the club.
+    * @param descr The description of the club.
+    */
+   private Club createClub(String name, String descr)
+   {
+      return new Club(name, descr, this);
+   }
+
+   /**
+    * Method to add a new administrator to a club.
+    * @param club The club that will be affected.
+    * @param admin The admin to be added.
+    */
+   private void addAdmin(Club club, ClubAdmin admin)
+   {
+      club.addAdmin(admin);
+   }
+
+   /**
+    * Method to delete an event.
+    * @param event The event to be deleted.
+    * @param club The club that the event belonged to.
+    */
+   private void deleteEvent(Event event, Club club)
+   {
+      club.deleteEvent(event);
+   }
+
+   /**
+    * Method to update an event.
+    * @param event The Event to be updated.
+    * @return The updated Event.
+    */
+   private Event updateEvent(Event event, String start, String end,
+       String loc, String descr)
+   {
+      Event curEvent = event;
+      curEvent.setStart(start);
+      curEvent.setEnd(end);
+      curEvent.setLocation(loc);
+      curEvent.setDescription(descr);
+      return curEvent;
+   }
+
+   /**
+    * Method to accept or reject member's request to join club.
+    * @param user The User that will be affected.
+    * @param club The Club that will be affected.
+    * @param accept The decision; true if accepted; false if rejected.
+    * @return Returns the decision of the admin.
+    */
+   private boolean acceptMember(User user, Club club, boolean accept)
+   {
+      if (accept)
+      {
+         club.addUser(user);
+      }
+      return accept;
+   }
+
+   /**
+    * Getter for adminID.
+    * @return Returns adminID.
+    */
+   public int getAdminID()
+   {
+      return adminID;
+   }
+}
