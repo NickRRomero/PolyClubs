678c575b43c775a7f0a1f5cded66fd9490c3a561
Nicolas Robert Romero <nromo01@gmail.com>
2016-04-28 17:07:43 -0700

    #512151 Working DatabaseManager

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -1,82 +1,111 @@
 package polyclubs.polyclubs;
 
 
+import android.os.AsyncTask;
+
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 import org.bson.Document;
+import org.json.JSONObject;
 
 /**
  * Created by Nick on 4/20/16.
  */
-public class DatabaseManager {
+public class DatabaseManager extends AsyncTask<Object, Void, JSONObject>
+{
 
-    String MongoDBConnectionIP;
-    boolean hasIPBeenSet = false;
+
     private String databaseName;
-    private String studentKey;
-    private String clubKey;
-    private boolean clubDBAccess;
-    private boolean studentDBAccess;
+    private MongoCollection<Document> documentCollection;
+    private Document singleDocument;
+    private String databaseURIToAccess = "";
+    private String collectionToRetrieve = "";
+    private String whichClubKey = "ClubName";
+    private String whichClubName = "";
+    private String whichStudentKey = "email";
+    private String whichStudentByEmail = "";
+    private boolean scopeOfDatabaseAccess = false;
+    private JSONObject profile = null;
+    public DatabaseManager() {
 
-    /**
-     * Constructor for a database object
-     * @param nicksLocalPolyIP Nick Romero's ip needed for the local system test in CPE309
-     */
-    public DatabaseManager(String nicksLocalPolyIP) {
-
-        if (hasIPBeenSet == false) {
-            MongoDBConnectionIP = nicksLocalPolyIP;
-            hasIPBeenSet = true;
-        }
 
     }
 
     /**
      * Determine which database needs to be access
-     * @param whichDatabase Either 'StudentDatabase' or 'ClubDatabase'
+     * @param whichDatabase Either 'studentDatabase' or 'clubDatabase'
+     * @param whichObject name of a student i.e. Nick Romero or name of club i.e. Society of Women Engineers
      */
-    public void setDataBaseDestination(String whichDatabase) {
+    public void setDataBaseDestination(String whichDatabase, String whichObject, boolean scopeOfAccess) {
         databaseName = whichDatabase;
-        setDatabaseAccess();
+        scopeOfDatabaseAccess = scopeOfAccess;
+        setDatabaseAccess(whichObject);
     }
-
 
     /**
      * Initialize the database accessing fields
      */
-    private void setDatabaseAccess() {
+    private void setDatabaseAccess(String whichObject) {
 
         if (databaseName.equals("StudentDatabase")) {
-            studentKey = "Students";
-            studentDBAccess = true;
-            clubKey = null;
-            clubDBAccess = false;
+            collectionToRetrieve = "students";
+            whichStudentByEmail = whichObject;
+            databaseURIToAccess = "mongodb://nick:password@ds017231.mlab.com:17231/studentdatabase";
         }
         else if (databaseName.equals("ClubDatabase")) {
-            clubKey = "Clubs";
-            clubDBAccess = true;
-            studentKey = null;
-            studentDBAccess = false;
+            collectionToRetrieve = "clubs";
+            whichClubName = whichObject;
+            databaseURIToAccess = "mongodb://nick:password@ds021691.mlab.com:21691/clubdatabase";
         }
+    }
+
+    @Override
+    protected JSONObject doInBackground(Object... params)
+    {
+
+        MongoClientURI uri = new MongoClientURI(databaseURIToAccess);
+        JSONObject jsonObject = new JSONObject();
+        MongoClient mongoClient = new MongoClient(uri);
+
+        MongoDatabase db = mongoClient.getDatabase(uri.getDatabase());
+        MongoCollection<Document> collection =  db.getCollection(collectionToRetrieve);
+
+
+        /**
+         * scopeOfAccess determines if a single document is returned from the database
+         * or a collection of documents
+         */
+        if (!scopeOfDatabaseAccess)
+        {
+            documentCollection = collection;
+        }
+        else
+        {
+            Iterable<Document> iterable = collection.find(eq("email",whichStudentByEmail));
+            Document doc = iterable.iterator().next();
+            profile = new JSONObject(doc);
+        }
+        return null;
     }
 
     /**
-     * Access the provided database given in the constructor.
-     * @return a JSON Document containing the database information
+     * Getter for database query
+     * @return documentCollection
      */
-    public Document accessDatabase() {
-        MongoClientURI uri = new MongoClientURI("mongodb://"+MongoDBConnectionIP+"/"+databaseName);
-        MongoClient mongoClient = new MongoClient(uri);
-        MongoDatabase db = mongoClient.getDatabase(uri.getDatabase());
-
-        FindIterable<Document> iter = db.getCollection("Clubs").find();
-        //TODO Determine what to return and when
-        return iter.first();
-
+    public MongoCollection<Document> getEntireDatabaseResults()
+    {
+        return documentCollection;
     }
 
+    /**
+     * Getter for a single database query
+     * @return profile a JSONObject
+     */
+    public JSONObject getSingleDatabaseResults()
+    {
+        return profile;
+    }
 }
-
