217497c9c14260b22ce85e4d2106d5abf8f25855
Nicolas Robert Romero <nromo01@gmail.com>
2016-05-05 16:52:35 -0700

    Changed Project to Console Interactions #512065

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -1,43 +1,77 @@
-package polyclubs.polyclubs;
+package polyclubsconsole;
 
 
-import android.os.AsyncTask;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
-
+import org.json.simple.*;
 import org.bson.Document;
-import org.json.JSONObject;
 
 /**
  * Created by Nick on 4/20/16.
  */
-public class DatabaseManager extends AsyncTask<Object, Void, JSONObject>
+public class DatabaseManager 
 {
-
-
+    /**Database Instance*/
+    private static DatabaseManager instance;
+    
+    /**Database Name to access*/
     private String databaseName;
+    
+    /**Collection returned from Database Query*/
     private MongoCollection<Document> documentCollection;
+    
+    /**Which MLAB Database to access*/
     private String databaseURIToAccess = "";
+    
+    /**Which collection to retrieve, user or club*/
     private String collectionToRetrieve = "";
+    
+    /**Student to look for*/
     private String whichStudentByEmail = "";
+    
+    /**How large of a collection to retrieve from MLAB*/
     private boolean scopeOfDatabaseAccess = false;
+    
+    /**Profile holding a single document from query*/
     private JSONObject profile = null;
 
-    public DatabaseManager() 
+    /**
+     * Constructor for DatabaseManager instance
+     */
+    private DatabaseManager() 
     {
       /**
-       * No paramter needed. The object return has the needed API.
+       * No parameter needed. The object return has the needed API.
        */
     }
+    
+    /**
+     * Create a singleton instance of DatabaseManager.
+     */
+    public static synchronized DatabaseManager getInstance()
+    {
+        /**
+         * Determine if the class has already been created
+         */
+        if (instance == null)
+        {
+            instance = new DatabaseManager();
+        }
+        
+        return instance;
+        
+    }
+    
       
     /**
      * Determine which database needs to be access
      * @param whichDatabase Either 'studentDatabase' or 'clubDatabase'
      * @param whichObject name of a student i.e. Nick Romero or name of club i.e. Society of Women Engineers
+     * @param scopeOfAccess grab an entire document or a single document
      */
     public void setDataBaseDestination(String whichDatabase, String whichObject, boolean scopeOfAccess) {
         databaseName = whichDatabase;
@@ -48,31 +82,35 @@ public class DatabaseManager extends AsyncTask<Object, Void, JSONObject>
     /**
      * Initialize the database accessing fields
      */
-    private void setDatabaseAccess(String whichObject) {
+    private void setDatabaseAccess(String whichObject) 
+    {
 
-        if ("StudentDatabase".equals(databaseName)) {
+        if ("StudentDatabase".equals(databaseName)) 
+        {
             collectionToRetrieve = "students";
             whichStudentByEmail = whichObject;
             databaseURIToAccess = "mongodb://nick:password@ds017231.mlab.com:17231/studentdatabase";
         }
-        else if ("ClubDatabase".eqauls(databaseName)) {
+        else if ("ClubDatabase".equals(databaseName)) 
+        {
             collectionToRetrieve = "clubs";
-            whichClubName = whichObject;
             databaseURIToAccess = "mongodb://nick:password@ds021691.mlab.com:21691/clubdatabase";
         }
     }
 
-    @Override
-    protected JSONObject doInBackground(Object... params)
+    /**
+     * Access the database and query the selected information*
+     */
+    protected void accessDatabase()
     {
-
+        System.err.close();
         MongoClientURI uri = new MongoClientURI(databaseURIToAccess);
         MongoClient mongoClient = new MongoClient(uri);
-
+        
         MongoDatabase db = mongoClient.getDatabase(uri.getDatabase());
         MongoCollection<Document> collection =  db.getCollection(collectionToRetrieve);
 
-
+        
         /**
          * scopeOfAccess determines if a single document is returned from the database
          * or a collection of documents
@@ -87,7 +125,6 @@ public class DatabaseManager extends AsyncTask<Object, Void, JSONObject>
             Document doc = iterable.iterator().next();
             profile = new JSONObject(doc);
         }
-        return null;
     }
 
     /**
@@ -108,3 +145,4 @@ public class DatabaseManager extends AsyncTask<Object, Void, JSONObject>
         return profile;
     }
 }
+
--- Login.java
+++ Login.java
@@ -1,66 +1,137 @@
-package polyclubs.polyclubs;
+package polyclubsconsole;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.concurrent.ExecutionException;
-
-public class Login extends Activity {
-
-    DatabaseManager databaseManager;
-    private static final String incorrectLoginErrorMessage = "Incorrect email and/or password.";
-    String loginText; 
-    String passwordText;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+import static java.lang.Thread.sleep;
+import java.util.Scanner;
+import org.json.simple.*;
+/**
+ *
+ * @author Nick
+ */
+public class Login 
+{
+    /**Student's email address*/
+    private String studentEmail = "";
+    
+    /**Student's password*/
+    private String studentPassword = "";
+    
+    /**Console Prompt for the Student Email*/
+    private static final String emailPrompt = "Enter your CalPoly Email "
+            + "Address: \nexample@calpoly.edu\n";
+    
+    /**Console Prompt for the Student password*/
+    private static final String passwordPrompt = "Enter your CalPoly Password:\n";
+    
+    /**Error message for logging in*/
+    private static final String errorLogin = "Incorrect email or password.\n";
+    
+    private boolean successfullLogin = false;
+    
+    /**Scanner to capture user information*/
+    private Scanner scanner;
+    
+    /**
+     * Display Login Screen to console;
+     */
+    public Login()
+    {
+       /**
+        * Only need object to instantiate displayWelcomeScreen
+        */
     }
 
-    public void confirmStudentInformation(View v) {
-
-        String key;
-        loginText = (EditText) findViewById(R.id.loginUserName);
-        passwordText = (EditText) findViewById(R.id.loginPassword);
-        JSONObject userProfile = null;
-        databaseManager = new DatabaseManager();
-        TextView errorMessage = (TextView) findViewById(R.id.errorMessage);
+    /**
+     * Display the login screen to the console.
+     */
+    public void displayWelcomeScreen() throws InterruptedException 
+    {
+        System.out.println("Poly\n    Clubs\n");
+        System.out.println("Please enter Login Credentials or type EXIT in email "
+                + "field to close the application.\n");
         /**
-         * This needs to be determined by the individual classes.
+         * Continuously prompt user for correct information.
          */
-
-        databaseManager.setDataBaseDestination("StudentDatabase", loginText.getText().toString(), false);
-        try {
-            userProfile = databaseManager.execute().get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+        while (!successfullLogin)
+        {
+            displayLoginRequests();
         }
-        if (userProfile != null) {
-            try {
-                key = userProfile.getString("key");
-                Log.e("PASSWORD", key);
-            } catch (JSONException e) {
-                Log.e("JSON EXCEPTION", e.toString());
+    }
+    
+    /**
+     * Display login requests.
+     */
+    private void displayLoginRequests() throws InterruptedException
+    {
+        scanner = new Scanner(System.in);
+        System.out.println(emailPrompt);
+        studentEmail = scanner.next();
+        
+        /**
+         * Determine if the user decided to exit the system.
+         * TODO Maybe IX
+         */
+        if ("EXIT".equals(studentEmail))
+        {
+            System.exit(0);
+        }
+        
+        System.out.println(passwordPrompt);
+        studentPassword = scanner.next();
+        confirmStudentInformation(studentEmail, studentPassword);
+    }
+    
+    /**
+     * Confirm the entered student information
+     * @param user username
+     * @param pass password
+     * @return true if student entered correct information
+     */
+    private void confirmStudentInformation(String user, String pass) throws InterruptedException
+    {
+        /**Object to hold returned user data.*/
+        JSONObject userProfile = null;
+        
+        /**Database Manager Object used to access mlab.com*/
+        DatabaseManager databaseManager = DatabaseManager.getInstance();
+        
+        /**Comparison key for returned profile*/
+        String tempKey;
+        
+        databaseManager.setDataBaseDestination("StudentDatabase", user, true);
+        
+        databaseManager.accessDatabase();
+        sleep(12000L);
+        userProfile = databaseManager.getSingleDatabaseResults();
+        /**Determine if the user was found in the database and/or entered 
+         * incorrect email.
+         */
+        if (userProfile != null)
+        {
+            
+            tempKey = userProfile.get("key").toString();
+            
+            /**
+             * Confirmed user password
+             */
+            if (!tempKey.equals(pass))
+            {
+                repromptStudentLogin();
             }
-            errorMessage.setVisibility(v.INVISIBLE);
         }
         else
         {
-
-            errorMessage.setVisibility(v.VISIBLE);
+            repromptStudentLogin();
         }
-
-
-
+        
+        successfullLogin = true;
     }
 
+    /**
+     * Re-prompt for more user information.
+     */
+    private void repromptStudentLogin() throws InterruptedException 
+    {
+        System.out.println(errorLogin);
+        displayLoginRequests();
+    }
 }
