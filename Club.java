590ca1c6252c56896e6c3d77893a3d93346156cb
hharaguc <holly.haraguchi@yahoo.com>
2016-05-06 22:36:53 -0700

    #512192, #512194 Added getters and setters to Club and Event classes

--- Club.java
+++ Club.java
@@ -1,82 +1,146 @@
+/** 
+ * @author Holly
+ * Represents a Cal Poly campus club
+ */
+
+import java.util.*;
+
 public class Club
 {
-	private String name;
-	private String descrip;
-	private ArrayList<ClubAdmin> admins;
-	private ArrayList<User> members;
-	private ArrayList<Event> clubEvents;
-	private ArrayList<User> userRequests;
-	private boolean areRequests;
+   private String name;
+   private String descrip;
+   private ArrayList<ClubAdmin> admins;
+   private ArrayList<User> members;
+   private ArrayList<Event> clubEvents;
+   private ArrayList<User> userRequests;
+   private boolean areRequests;
 
-	public Club(String nm, String des, ClubAdmin admin)
-	{
-		name = nm;
-		descrip = des;
-		admins = new ArrayList<ClubAdmin>();
-		admins.add(admin);
-		members = new ArrayList<User>();
-		clubEvents = new ArrayList<Event>();
-		userRequests = new ArrayList<User>();
-		areRequests = False;
-	}
+   public Club(String nm, String des, ClubAdmin admin)
+   {
+      name = nm;
+      descrip = des;
+      admins = new ArrayList<ClubAdmin>();
+      admins.add(admin);
+      members = new ArrayList<User>();
+      clubEvents = new ArrayList<Event>();
+      userRequests = new ArrayList<User>();
+      areRequests = false;
+   }
 
-	private void editDescription(String newDescrip)
-	{
-		descrip = newDescrip;
-	}
+    // Club getters
+    public String getName()
+    {
+        return name;
+    }
 
-	// Club admin functions
-	private boolean addAdmin(ClubAdmin admin)
-	{
-		return admins.add(admin);
-	}
+    public String getDescrip()
+    {
+        return descrip;
+    }
 
-	private boolean deleteAdmin(ClubAdmin admin)
-	{
-		return admins.remove(admin);
-	}
+    public ArrayList<ClubAdmin> getAdmins()
+    {
+        return admins;
+    }
 
-	// Member request functions
-	private boolean addRequest(User user)
-	{
-		areRequests = True;
-		return userRequests.add(user);
-	}
+    public ArrayList<User> getMembers()
+    {
+        return members;
+    }
 
-	private boolean removeRequest(User user)
-	{
-		boolean val = False;
-		val = userRequests.remove(user);
-		areRequests = !userRequests.isEmpty();
+    public ArrayList<Event> getEvents()
+    {
+        return clubEvents;
+    }
 
-		return val;
-	}
+    // Club setters
+    public void setName(String newName)
+    {
+        name = newName;
+    }
 
-	// Club event functions
-	private boolean addEvent(Event event)
-	{
-		return clubEvents.add(event);
-	}
+   public void setDescription(String newDescrip)
+   {
+      descrip = newDescrip;
+   }
 
-	private boolean removeEvent(Event event)
-	{
-		return clubEvents.remove(event);
-	}
+   // Club admin functions
+   public boolean addAdmin(ClubAdmin admin)
+   {
+      return admins.add(admin);
+   }
 
-	private void editEvent(Event event, int sh, int sm, int eh, int em,
-		String loc, String desc)
-	{
-		event.updateEvent(sh, sm, eh, em, loc, desc);
-	}
+   public boolean deleteAdmin(ClubAdmin admin)
+   {
+      return admins.remove(admin);
+   }
 
-	// Club membership functions
-	private boolean addMember(User user)
-	{
-		return members.add(user);
-	}
+   // Member request functions
+   public boolean addRequest(User user)
+   {
+      areRequests = true;
+      return userRequests.add(user);
+   }
 
-	private boolean removeMember(User user)
-	{
-		return members.remove(user);
-	}
+   public boolean removeRequest(User user)
+   {
+      boolean val = false;
+      val = userRequests.remove(user);
+      areRequests = !userRequests.isEmpty();
+
+      return val;
+   }
+
+   // Club event functions
+   public boolean addEvent(Event event)
+   {
+      return clubEvents.add(event);
+   }
+
+   public boolean removeEvent(Event event)
+   {
+      return clubEvents.remove(event);
+   }
+
+   public void editEvent(Event event, int sh, int sm, int eh, int em,
+      String loc, String desc)
+   {
+      event.updateEvent(sh, sm, eh, em, loc, desc);
+   }
+
+   // Club membership functions
+   public boolean addMember(User user)
+   {
+      return members.add(user);
+   }
+
+   public boolean removeMember(User user)
+   {
+      return members.remove(user);
+   }
+
+   // Prints all club information
+   public void printClubInfo()
+   {
+      System.out.println("Club name: " + name);
+      System.out.println("Club description: " + descrip);
+      
+      System.out.println("Club admins: ");
+      for (int i = 0; i < admins.size(); i++)
+      {
+         System.out.println("   -" + admins.get(i));
+      }
+
+      System.out.println("Club members: ");
+      for (int i = 0; i < members.size(); i++)
+      {
+         System.out.println("   -" + members.get(i));
+      }
+
+        System.out.println("Club events: ");
+        for (int i = 0; i < clubEvents.size(); i++)
+        {
+            System.out.println("   -" + clubEvents.get(i));
+        }
+   }
 }
--- Event.java
+++ Event.java
@@ -1,50 +1,89 @@
-// Represents a club event
-// Event start and end times based on a 24-hour clock
+/** 
+ * @author Holly
+ * Represents a club event
+ * Event start and end times based on a 24-hour clock
+ */
+
+import java.util.*;
 
 public class Event
 {
-	private int startHour;
-	private int startMinute;
-	private int endHour;
-	private int endMinute;
-	private String location; // location of the event
-	private String description; // description of the event
-	private ArrayList<User> going; // users going to the event
+   private int startHour;
+   private int startMinute;
+   private int endHour;
+   private int endMinute;
+   private String location; // location of the event
+   private String descrip; // description of the event
+   private ArrayList<User> going; // users going to the event
 
-	public Event(int sh, int sm, int eh, int em, String loc, String descrip)
-	{
-		startHour = sh;
-		startMinute = sm;
-		endHour = eh;
-		endMinute = em;
-		location = loc;
-		description = descrip;
-		going = new ArrayList<User>();
-	}
+   public Event(int sh, int sm, int eh, int em, String loc, String description)
+   {
+      startHour = sh;
+      startMinute = sm;
+      endHour = eh;
+      endMinute = em;
+      location = loc;
+      descrip = description;
+      going = new ArrayList<User>();
+   }
 
-	private void addMember(User user)
-	{
-		going.add(user);
-	}
+   // Event getters
+   public String getStartTime()
+   {
+      return Integer.toString(startHour) + ':' + Integer.toString(startMinute);
+   } 
 
-	private void deleteMember(User user)
-	{
-		going.remove(user);
-	}
+   public String getEndTime()
+   {
+      return Integer.toString(endHour) + ':' + Integer.toString(endMinute);
+   }
 
-	private ArrayList<User> getGoing()
-	{
-		return going;
-	}
+   public String getLoc()
+   {
+      return location;
+   }
 
-	private void updateEvent(int sh, int sm, int eh, int em, String loc, 
-		String descrip)
-	{
-		startHour = sh;
-		startMinute = sm;
-		endHour = eh;
-		endMinute = em;
-		location = loc;
-		description = descrip;
-	}
+   public String getDescrip()
+   {
+      return descrip;
+   }
+
+   public ArrayList<User> getGoing()
+   {
+      return going;
+   }
+
+   // Event setters
+   public void setStartTime(int sh, int sm)
+   {
+      startHour = sh;
+      startMinute = sm;
+   }
+
+   public void setEndTime(int eh, int em)
+   {
+      endHour = eh;
+      endMinute = em;
+   }
+
+   public void setLoc(String loc)
+   {
+      location = loc;
+   }
+
+   public void setDescrip(String description)
+   {
+      descrip = description;
+   }
+
+   // Event attendee functions
+   public void addMember(User user)
+   {
+      going.add(user);
+   }
+
+   public void deleteMember(User user)
+   {
+      going.remove(user);
+   }
 }
