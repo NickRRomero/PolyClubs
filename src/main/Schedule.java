006ad1651f1637e452cc1ab4fb78808bcf3afbec
jacobking33 <jm.king@comcast.net>
2016-05-08 16:40:47 -0700

    IDs: 540429, 540421, 512134

--- src/main/Schedule.java
+++ src/main/Schedule.java
@@ -1,33 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polyclubsconsole;
+
 import java.util.ArrayList;
 
 /**
- * Created by jacob on 4/24/2016.
+ *
+ * @author jacob
  */
 public class Schedule {
-    private ArrayList<Object> schedule = new ArrayList<Object>();
+   private ArrayList<Object> schedule = new ArrayList<Object>();
 
-    // add a course to a student's schedule
-    public void addCourse(Course course) {
-        schedule.add(course);
-    }
+   // add a course to a student's schedule
+   public void addCourse(Course course) {
+       schedule.add(course);
+   }
 
-    // remove a course from a student's schedule
-    public void removeCourse(Course course) {
-        schedule.remove(course);
-    }
+   // remove a course from a student's schedule
+   public void removeCourse(Course course) {
+       schedule.remove(course);
+   }
 
-    // determine if event conflicts with class schedule
-    public void hasConflict(Event event) {
+   // determine if event conflicts with class schedule
+   public void hasConflict(Event event) {
 
-    }
+   }
 
-    // add an event to the schedule
-    public void addEvent(Event event) {
-        schedule.add(event);
-    }
+   // add an event to the schedule
+   public void addEvent(Event event) {
+       schedule.add(event);
+   }
 
-    // remove event from schedule
-    public void removeEvent(Event event) {
-        schedule.remove(event)
-    }
+   // remove event from schedule
+   public void removeEvent(Event event) {
+       schedule.remove(event);
+   }
 }
+
