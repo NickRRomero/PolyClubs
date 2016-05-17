ea11c91c58103b29eaae91115ca2d0df69a6b945
Nicolas Robert Romero <nromo01@gmail.com>
2016-05-16 20:21:26 -0700

    Update ClubSearch #513134

--- src/main/ClubSearch.java
+++ src/main/ClubSearch.java
@@ -1,106 +1,100 @@
-package com.example.myapplication;
 
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+package polyclubsconsole;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.util.JSON;
+import java.util.Scanner;
+import org.bson.Document;
+import org.json.JSONObject;
 
-public class ClubSearch extends AppCompatActivity {
+/**
+ *
+ * @author jacob
+ */
+public class ClubSearch 
+{ 
+   
+   private final Scanner in;
+   
+   private static ClubSearch clubSearch;
+   
+   private ClubSearch() 
+   {
+      in = new Scanner(System.in);
+      displaySearch();
+   }
+   
+   public static synchronized ClubSearch getInstance()
+   {
+       if (clubSearch == null)
+       {
+           clubSearch = new ClubSearch();
+       }
+       
+       return clubSearch;
+   }
+   
+   private void displaySearch() {
+      System.out.println("Please enter the name of the club " +
+       "you would like to search for or 'exit' to exit search:");
+      
+      // club name that user searches for
+      String club = in.nextLine();
+      
+      if ("exit".equalsIgnoreCase(club))
+         ;///////////////
+      else
+         searchClub(club);
+   }
+   
+   private void searchClub(String club){
+      /**Database Manager Object used to access mlab.com*/
+      DatabaseManager databaseManager = DatabaseManager.getInstance();
+      databaseManager.setDataBaseDestination("ClubDatabase", club, true);
+      databaseManager.accessDatabase();
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.myapplication/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.myapplication/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
+      JSONObject clubName = databaseManager.getSingleDatabaseResults();
+      //filterClubs(databaseManager);
+      
+      if(clubName != null) {
+         String name = clubName.get("ClubName").toString();
+         viewPage(name);
+      }
+      else {
+         System.out.println("The club you entered is " +
+          "not in the database.\n");
+         displaySearch();
+      } 
+   }
+   
+   private void filterClubs(DatabaseManager db) {
+      MongoCollection<Document> col = db.getEntireDatabaseResults();
+      Iterable<Document> iter;
+      iter = col.find();
+      
+      for (Document doc : iter) {
+         JSONObject profile = new JSONObject(doc);
+         System.out.println(profile.get("ClubName").toString());
+      }
+   }
+   
+   private void viewPage(String club) {
+      System.out.println("Would you like to view the " + 
+       club + " clubpage? (y/n)");
+      
+      if("y".equalsIgnoreCase(in.nextLine())) 
+         ;// club page
+      else {
+         System.out.println();
+         displaySearch(); 
+      }
+   }
+   
+   /**
+   public static void main(String[] args) {
+      ClubSearch ds = new ClubSearch();
+      ds.displaySearch();
+   }*/
 }
+
