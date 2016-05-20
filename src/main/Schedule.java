6628e30ff6ddc0a4f622b54d6b784f65fd6b1c98
jacobking33 <jm.king@comcast.net>
2016-05-19 17:49:50 -0700

    #547013

--- src/main/Schedule.java
+++ src/main/Schedule.java
@@ -1,42 +1,125 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package polyclubsconsole;
+package Logic;
 
 import java.util.ArrayList;
 
 /**
- *
- * @author jacob
+ * Created by jacob on 4/24/2016.
  */
 public class Schedule {
-   private ArrayList<Object> schedule = new ArrayList<Object>();
+    private ArrayList<Object> schedule;
+    
+    public Schedule() {
+    	schedule = new ArrayList<Object>();
+    }
 
-   // add a course to a student's schedule
-   public void addCourse(Course course) {
-       schedule.add(course);
-   }
+    // add course or event to schedule
+    public void add(Object obj) {
+    	schedule.add(obj);
+    }
+    
+    // remove course or event from schedule
+    public void remove(Event event) {
+        schedule.remove(event);
+    }
+    
+    // clear schedule
+    public void clear() {
+    	schedule.clear();
+    }
+    
+    // determine if an event conflicts with a user's schedule
+    public boolean hasConflict(Event event) {
+    	// variables to be set later
+    	Course c = null;
+    	Event e = null;
+    	Time stime = null;
+    	Time etime = null;
+    	boolean sameDay = false;
+    	
+    	// loop through each event/course in schedule
+    	outerloop: 
+    	for (Object ec : schedule) {
+    		System.out.println("Event");
+    		// if course, determine if event and course are on same day
+    		if (ec instanceof Course) {
+    			c = (Course) ec;
+    			for(String day : c.getDays()) {
+    				// if on same day, get time and exit outerloop
+    				if (event.getDay().equals(day)) {
+    					sameDay = true;
+    					stime = c.getStart();
+    					etime = c.getEnd();
+    					break outerloop;
+    				}
+    			}
+    		}
+    		// if event, determine if events are on same date
+    		else {
+    			e = (Event) ec;
+    			// if on same date, get time and exit outerloop
+	    		if (event.getDate().equals(e.getDate())) {
+					sameDay = true;
+					stime = e.getStartTime();
+					etime = e.getEndTime();
+					break outerloop;
+	    		}
+    		}	
+    	}
+    	
+    	if (sameDay) 
+    		System.out.println("Event: " + stime + " " + etime);
+    	
+    	return true;
+    }
+    
+    public ArrayList<Course> getCourses() {
+    	ArrayList<Course> courses = new ArrayList<Course>();
+    	
+    	for (Object obj : schedule) {
+    		if (obj instanceof Course) 
+    			courses.add((Course) obj);
+    	}
+    	
+    	return courses;
+    }
+    
+    public void displayWeek() {
+    	;
+    }
+    
+    static public void main(String[] args) {
+    	Date pd = new Date(5, 15);
+    	Time pst = new Time(13, 00);
+    	Time pet = new Time(15, 30);
+    	
+    	Date nd = new Date(5, 13);
+    	Time nst = new Time(14, 10);
+    	Time net = new Time(15, 05);
+    	
+    	Date pid = new Date(5, 14);
+    	Time pist = new Time(12, 00);
+    	Time piet = new Time(14, 25);
+    	
+    	Time sst = new Time(11, 10);
+    	Time set = new Time(13, 00);
+    	
+    	Event party = new Event("W", pid, pst, pet, "Birthday Party");
+    	Event nap = new Event("M", nd, nst, net, "Take a nap");
+    	Event eat = new Event("Sa", pid, pist, piet, "pizza time");
+    	Course math = new Course("math", nst, net, "M_T_W_R");
+    	Course science = new Course("science", sst, set, "M_W_F");
+    	
+    	Schedule sch = new Schedule();
+    	
+    	sch.add(math);
+    	sch.add(nap);
+    	sch.add(science);
+    	sch.add(party);
+    	System.out.println(sch.schedule.size() + "\n");
+    	System.out.println(nap.getEndTime().toString());
 
-   // remove a course from a student's schedule
-   public void removeCourse(Course course) {
-       schedule.remove(course);
-   }
-
-   // determine if event conflicts with class schedule
-   public void hasConflict(Event event) {
-
-   }
-
-   // add an event to the schedule
-   public void addEvent(Event event) {
-       schedule.add(event);
-   }
-
-   // remove event from schedule
-   public void removeEvent(Event event) {
-       schedule.remove(event);
-   }
+    	sch.hasConflict(eat);
+    	
+    }
 }
 
