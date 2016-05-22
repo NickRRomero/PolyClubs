ed876cb5ad3bbf24ecd9a8c96132678608d7ec6b
Nicolas Robert Romero <nromo01@gmail.com>
2016-05-22 15:08:23 -0700

    Update PolyClubsConsole Navigation #512200

--- src/main/PolyClubsConsole.java
+++ src/main/PolyClubsConsole.java
@@ -1,21 +1,37 @@
 package polyclubsconsole;
 
+import org.json.JSONObject;
+
 /**
  *
  * @author Nick
  */
 public class PolyClubsConsole 
 {
-
+    /**User of the application at run time*/
+    protected static User user = null;
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws InterruptedException 
+    public static void main(String[] args) throws InterruptedException, Exception 
     {
+       
+        
         Login login = new Login();
+        ClubPrompts clubPrompts = new ClubPrompts();
         login.displayWelcomeScreen();
-        //TODO Switch to Logged in Screen
+        JSONObject objec = login.getStudentProfile();
+        user = login.setupUser(objec);
+        System.out.println(user.getEmail());
+        System.out.println(user.getName());
+        System.out.println(user.getPhoneNum());
+        System.out.println(user.getClass());
+        clubPrompts.displayClubPrompt();
+        
+        
     }
     
     
+   
 }
