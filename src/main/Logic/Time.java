2222ed6723f0fdc4d020f82e0b9ac557af9a5cc4
hharaguc <holly.haraguchi@yahoo.com>
2016-05-24 17:31:48 -0700

    #512119 Added toString() method

--- src/main/Logic/Time.java
+++ src/main/Logic/Time.java
@@ -1,12 +1,12 @@
-
-ckage Logic;
+package Logic;
 
 /** 
  * @author Holly
  * Represents an event time
  */
 
-public class Time {
+public class Time 
+{
    private int hour;
    private int minute;
 
@@ -57,53 +57,3 @@ public class Time {
       return String.format("%02d", hour) + ":" + String.format("%02d", minute);
    }
 }
-
-/ckage Logic;
-
-/** 
- * @author Holly
- * Represents an event date
- */
-
-public class Date {
-	private int month;
-	private int day;
-
-	public Date(int month, int day)
-	{
-		this.month = month;
-      this.day = day;
-    }
-
-   /**
-    * Method to order dates
-    * @param d2 - Date to compare to 'this'
-    * @return true if 'this' is earlier than or equal to d2
-    */
-   public boolean isEarlier(Date d2)
-   {
-      if (this.month <= d2.month)
-      {
-         if (this.day <= d2.day)
-         {
-            return true;
-         }
-      }
-      return false;
-   }
-
-   // Date setters
-   public void setDate(int month, int day)
-   {
-      this.month = month;
-      this.day = day;
-   }
-
-   // Date getters
-   public int getMonth()
-   {
-      return month;
-   } 
-
-   public int getDay()
-   {
